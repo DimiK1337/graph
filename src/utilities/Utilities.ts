41dab8798a33e7148e23114a2184a6e01e7f0f84
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-24 10:11:18 -0700

    UTILITIES: Utilities.getRandomElement(array) ReturnsRandomElementFromArray -> PASS

--- src/utilities/Utilities.ts
+++ src/utilities/Utilities.ts
@@ -1,3 +1,5 @@
+import { UUID } from "../Types.js";
+
 /**
  * Example: "c4076ede-bddf-47f3-8237-5712b4d3eda6"
  */
@@ -9,4 +11,7 @@ export class Utilities {
     /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
       uuid
     );
+
+  public static getRandomElement = <T>(array: T[]): T =>
+    array[Math.floor(Math.random() * array.length)];
 }
