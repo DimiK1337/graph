d25c5b40ffa7d31e1e841612e1e53fbfffdc9e3d
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-24 09:05:38 -0700

    UTILITIES: Utilities.uuid ReturnsValidUUID -> PASS

---
+++ src/utilities/Utilities.ts
@@ -0,0 +1,12 @@
+/**
+ * Example: "c4076ede-bddf-47f3-8237-5712b4d3eda6"
+ */
+export class Utilities {
+  public static get uuid() {
+    return crypto.randomUUID();
+  }
+  public static isUUID = (uuid: string): uuid is UUID =>
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+      uuid
+    );
+}
