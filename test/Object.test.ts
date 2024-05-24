1ca249d843197c8e3ebcac4565d13ff5e3af7d1e
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-24 10:39:40 -0700

    OBJECT: Object.extend(node, metadata) ReturnsNodeWithMetadata -> FAIL

--- test/Object.test.ts
+++ test/Object.test.ts
@@ -46,5 +46,54 @@ describe("Given Object imported", () => {
         expect(node.icon).toBe(details.icon);
       });
     });
+    it("then Object has a extend method", () => {
+      expect(Object.extend).toBeDefined();
+    });
+    describe("when Object.extend(node, metadata) is called", () => {
+      let node;
+      let metadata;
+      beforeEach(() => {
+        node = Object.create({
+          name: "Node",
+          type: Utilities.getRandomElement<NodeType>(NodeTypes),
+          coordinates: { x: 0, y: 0 },
+          icon: "./icon.svg",
+        });
+        metadata = {
+          arrival: {
+            distribution: "exponential",
+            parameters: [{ rate: 1 }],
+          },
+        };
+      });
+      it("then an extended node is return", () => {
+        let extendedNode = Object.extend(node, metadata);
+        expect(extendedNode).toBeDefined();
+      });
+      it("then extended node has an id", () => {
+        let extendedNode = Object.extend(node, metadata);
+        expect(extendedNode.id).toBe(node.id);
+      });
+      it("then extended node has a name", () => {
+        let extendedNode = Object.extend(node, metadata);
+        expect(extendedNode.name).toBe(node.name);
+      });
+      it("then extended node has a type", () => {
+        let extendedNode = Object.extend(node, metadata);
+        expect(extendedNode.type).toBe(node.type);
+      });
+      it("then extended node has coordinates", () => {
+        let extendedNode = Object.extend(node, metadata);
+        expect(extendedNode.coordinates).toBe(node.coordinates);
+      });
+      it("then extended node has an icon", () => {
+        let extendedNode = Object.extend(node, metadata);
+        expect(extendedNode.icon).toBe(node.icon);
+      });
+      it("then extended node has metadata", () => {
+        let extendedNode = Object.extend(node, metadata);
+        expect(extendedNode.metadata).toBe(metadata);
+      });
+    });
   });
 });
