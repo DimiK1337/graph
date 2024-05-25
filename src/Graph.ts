77a8f00a1ba6f71890279ce54289640e381dbaa2
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-25 07:27:24 -0700

    GRAPH: findNodeById(nodes, id) ReturnsNodeWithMatchingId -> PASS

--- src/Graph.ts
+++ src/Graph.ts
@@ -1,6 +1,8 @@
 import { Node } from "./Types.js";
 import { Object } from "object";
 import { Tuple } from "tuple";
+import { ObjectNode } from "./Object.types.js";
+import { TupleNode } from "./Tuple.types.js";
 
 type NodeFactory = typeof Object | typeof Tuple;
 
@@ -9,9 +11,13 @@ export class Graph {
   constructor(nodeFactory: NodeFactory) {
     this.nodeFactory = nodeFactory;
   }
-  addNode(nodes: Node[], { name, type, coordinates, icon }) {
+  addNode = (nodes: Node[], { name, type, coordinates, icon }) => {
     const node = this.nodeFactory.create({ name, type, coordinates, icon });
     nodes.push(node);
     return nodes;
-  }
+  };
+  findNodeById = (nodes: Node[], id: string) =>
+    this.nodeFactory.structure === "object"
+      ? nodes.find((node: ObjectNode) => node.id === id)
+      : nodes.find((node: TupleNode) => node[0] === id);
 }
