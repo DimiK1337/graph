905c8e6fec5bc1d7367f71946a8dc216743f03b1
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-25 07:30:35 -0700

    DOCUMENTATION: Add Refactor Comment

--- src/Graph.ts
+++ src/Graph.ts
@@ -6,6 +6,9 @@ import { TupleNode } from "./Tuple.types.js";
 
 type NodeFactory = typeof Object | typeof Tuple;
 
+// Operations on Graph takes nodes as first argument to enable performance testing
+// Once performance testing is done, we can refactor to use a class instance
+// Also operations can return this to enable fluent interface
 export class Graph {
   public nodeFactory: NodeFactory;
   constructor(nodeFactory: NodeFactory) {
